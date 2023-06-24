301f3f712307777db267cb20ab2a8e3e9e239bed
hamroy <hamroy1228@gmail.com>
2023-06-24 13:20:33 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      const { name, location } = request.query;
+      return `Hello, ${name} from ${location}`;
+    },
+  },
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
